60e1a48e915987085aa440c0843e8e9eb80b10c4
magarsahil <sahilmagar275@gmail.com>
2026-07-04 14:28:08 +0545

    Sucessfully Login into the Admin panel with it's database table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin;
+// use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,14 +13,21 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    // public function run(): void
+    // {
+    //     // Admin::factory(10)->create();
+
+    //     Admin::factory()->create([
+    //         'name' => 'Test Admin',
+    //         'email' => 'admin@gmail.com',
+    //         'password' => 'password',
+    //     ]);
+    // }
+
     public function run(): void
     {
-        // Admin::factory(10)->create();
-
-        Admin::factory()->create([
-            'name' => 'Test Admin',
-            'email' => 'admin@gmail.com',
-            'password' => 'password',
+        $this->call([
+            AdminSeeder::class,
         ]);
     }
 }
