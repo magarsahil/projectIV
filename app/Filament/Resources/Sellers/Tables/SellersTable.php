8412919c34cc138be6efcb19de670f956b4197bc
magarsahil <sahilmagar275@gmail.com>
2026-07-11 20:58:38 +0545

    Filament Resource Seller Design is Complete

---
+++ app/Filament/Resources/Sellers/Tables/SellersTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Resources\Sellers\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SellersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('shop_name')
+                    ->searchable(),
+                TextColumn::make('status')
+                    ->badge(),
+                TextColumn::make('expired_date')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('contact')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
