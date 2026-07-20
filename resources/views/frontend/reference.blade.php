4ed364da9eebca9d6d9e973e6fa90b9d21b2d3d5
magarsahil <sahilmagar275@gmail.com>
2026-07-20 22:01:54 +0545

    Reference page in frontend

---
+++ resources/views/frontend/reference.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layout')
+
+@section('styles')
+    <style>
+        /* ya chai Css matra Lekhna thau */
+    </style>
+@endsection
+
+@section('content')
+    {{-- Ya chai Content matra Lekhna thau --}}
+@endsection
+
+
+@section('scripts')
+    <script>
+        // ya chai Js matra Lekhna thau
+    </script>
+@endsection
